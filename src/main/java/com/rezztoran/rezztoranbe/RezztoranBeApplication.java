384c37a5e40c976f5698ed5bbca3e44497562d5a
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-11-18 20:42:21 +0300

    Initial commit

---
+++ src/main/java/com/rezztoran/rezztoranbe/RezztoranBeApplication.java
@@ -0,0 +1,13 @@
+package com.rezztoran.rezztoranbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezztoranBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RezztoranBeApplication.class, args);
+	}
+
+}

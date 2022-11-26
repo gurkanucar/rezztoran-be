df9ef86562d7693ee19caa4230679d8d6c3bbd94
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-11-26 23:23:23 +0300

    CategoryDTO created

---
+++ src/main/java/com/rezztoran/rezztoranbe/dto/CategoryDTO.java
@@ -0,0 +1,18 @@
+package com.rezztoran.rezztoranbe.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CategoryDTO {
+
+    private Long id;
+    private String categoryName;
+    private String categoryImage;
+
+}

0059d829088b786dc6bcc1884ff4382d8d35bab8
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-11-26 21:54:53 +0300

    open api implemented

---
+++ src/main/java/com/rezztoran/rezztoranbe/config/OpenApiConfig.java
@@ -0,0 +1,38 @@
+package com.rezztoran.rezztoranbe.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${application-description}") String description,
+                                 @Value("${application-version}") String version) {
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(
+                        new Components()
+                                .addSecuritySchemes(securitySchemeName,
+                                        new SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                )
+                .info(new Info().title("Rezztoran API")
+                        .version(version)
+                        .description(description)
+                        .license(new License().name("Rezztoran API Licence")));
+    }
+}

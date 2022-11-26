8341a380af917e45317ad2c970ba8862691bda5f
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-11-26 13:39:33 +0300

    food model created

---
+++ src/main/java/com/rezztoran/rezztoranbe/model/Food.java
@@ -0,0 +1,29 @@
+package com.rezztoran.rezztoranbe.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.*;
+import java.util.List;
+
+@SuperBuilder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Food extends BaseEntity {
+
+    private String foodName;
+    private String foodImage;
+
+    @ManyToMany
+    private List<Category> categories;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "menu_id", nullable = false)
+    private Menu menu;
+
+
+}

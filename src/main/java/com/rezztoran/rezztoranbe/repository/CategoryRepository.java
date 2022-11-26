8c395c9c9eab66592fe505fb4521bbdd6a14ba0d
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-11-26 23:36:44 +0300

    CategoryRepository updated

--- src/main/java/com/rezztoran/rezztoranbe/repository/CategoryRepository.java
+++ src/main/java/com/rezztoran/rezztoranbe/repository/CategoryRepository.java
@@ -4,6 +4,11 @@ import com.rezztoran.rezztoranbe.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+   Optional<Category> findByCategoryName(String name);
+
 }

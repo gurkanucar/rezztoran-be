3ad45b4df3be6253801464e4269b018832285a76
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-11-26 13:39:25 +0300

    comment model created

---
+++ src/main/java/com/rezztoran/rezztoranbe/model/Comment.java
@@ -0,0 +1,27 @@
+package com.rezztoran.rezztoranbe.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@SuperBuilder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Comment extends BaseEntity {
+
+    private String content;
+    private int star;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+}

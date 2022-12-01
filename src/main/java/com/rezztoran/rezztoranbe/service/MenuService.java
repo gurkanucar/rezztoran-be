b75cab1ce2d53d3b9acef43151b19845574bc055
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-12-01 22:50:30 +0300

    MenuService created

---
+++ src/main/java/com/rezztoran/rezztoranbe/service/MenuService.java
@@ -0,0 +1,38 @@
+package com.rezztoran.rezztoranbe.service;
+
+import com.rezztoran.rezztoranbe.model.Menu;
+import com.rezztoran.rezztoranbe.repository.MenuRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MenuService {
+
+    private final MenuRepository menuRepository;
+    private final RestaurantService restaurantService;
+
+    public Menu create(Menu menu) {
+        var restaurant = restaurantService.getById(menu.getRestaurant().getId());
+        menu.setRestaurant(restaurant);
+        return menuRepository.save(menu);
+    }
+
+    public Menu update(Menu menu) {
+        var restaurant = restaurantService.getById(menu.getRestaurant().getId());
+        var existing = getMenuById(menu.getId());
+        existing.setRestaurant(restaurant);
+        return menuRepository.save(existing);
+    }
+
+    public void delete(Long id) {
+        var existing = getMenuById(id);
+        menuRepository.delete(existing);
+    }
+
+    public Menu getMenuById(Long id) {
+        return menuRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("menu not found!"));
+    }
+
+}

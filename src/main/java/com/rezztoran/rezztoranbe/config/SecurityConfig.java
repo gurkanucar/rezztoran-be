75f4ebac574bf3469dafc6a18c43d70336d6aa40
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-12-07 19:33:54 +0300

    SecurityConfig implemented

---
+++ src/main/java/com/rezztoran/rezztoranbe/config/SecurityConfig.java
@@ -0,0 +1,84 @@
+package com.rezztoran.rezztoranbe.config;
+
+import com.rezztoran.rezztoranbe.security.JWTAccessDeniedHandler;
+import com.rezztoran.rezztoranbe.security.JwtAuthenticationEntryPoint;
+import com.rezztoran.rezztoranbe.security.JwtFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtFilter jwtFilter;
+    private final JwtAuthenticationEntryPoint authenticationEntryPoint;
+    private final JWTAccessDeniedHandler accessDeniedHandler;
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(final AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .headers().frameOptions().disable().and()
+                .csrf().disable()
+                .cors().and()
+                .authorizeRequests(auth -> {
+//                    auth.antMatchers("/api/admin").hasAuthority("ADMIN");
+//                    auth.antMatchers("/api/user").hasAnyAuthority("ADMIN", "USER");
+                    auth.anyRequest().authenticated();
+                })
+                .formLogin().disable()
+                .httpBasic().disable()
+                .exceptionHandling().accessDeniedHandler(accessDeniedHandler)
+                .authenticationEntryPoint(authenticationEntryPoint)
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers(
+                "/h2-console/**",
+                "/api/auth/**",
+                "/api/user/reset-request",
+                "/api/user/reset-password",
+                "/swagger-resources/**",
+                "/swagger-ui.html/**",
+                "/swagger-resources/**",
+                "/swagger-ui/**",
+                "/v3/api-docs/**");
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("*");
+            }
+        };
+    }
+}

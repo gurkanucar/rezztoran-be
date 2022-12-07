966e773c7a42208d614879c3c75be95928b5b924
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-12-07 19:33:26 +0300

    UserDetailsServiceImpl created

---
+++ src/main/java/com/rezztoran/rezztoranbe/service/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.rezztoran.rezztoranbe.service;
+
+import com.rezztoran.rezztoranbe.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.findUserByUsername(username);
+        var roles = Stream.of(user.getRole())
+                .map(x -> new SimpleGrantedAuthority(x.name()))
+                .collect(Collectors.toList());
+        return new org.springframework.security.core.userdetails.User(username, user.getPassword(), roles);
+    }
+}

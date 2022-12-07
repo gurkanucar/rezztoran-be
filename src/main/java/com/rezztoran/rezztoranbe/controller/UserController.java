e513c249b1684aa79c51582190975ccc7edd4401
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-12-07 19:39:13 +0300

    UserController created

---
+++ src/main/java/com/rezztoran/rezztoranbe/controller/UserController.java
@@ -0,0 +1,60 @@
+package com.rezztoran.rezztoranbe.controller;
+
+import com.rezztoran.rezztoranbe.dto.UserDTO;
+import com.rezztoran.rezztoranbe.dto.request.PasswordResetModel;
+import com.rezztoran.rezztoranbe.dto.request.PasswordResetRequest;
+import com.rezztoran.rezztoranbe.dto.request.RegisterModel;
+import com.rezztoran.rezztoranbe.model.User;
+import com.rezztoran.rezztoranbe.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final ModelMapper modelMapper;
+    private final UserService userService;
+
+    @PutMapping
+    public ResponseEntity<UserDTO> updateUser(@RequestBody User user) {
+        return ResponseEntity.ok(modelMapper.map(userService.update(user), UserDTO.class));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> updateUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/reset-request")
+    public ResponseEntity<Void> resetMail(@RequestBody PasswordResetRequest passwordResetRequest) {
+        userService.resetPasswordRequestCodeGenerate(passwordResetRequest.getMail());
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/reset-password")
+    public ResponseEntity<Void> resetPassword(@RequestBody PasswordResetModel passwordResetModel) {
+        userService.resetPassword(passwordResetModel);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> getUsers() {
+        return ResponseEntity.ok(userService.getUsers().stream()
+                .map(x -> modelMapper.map(x, UserDTO.class))
+                .collect(Collectors.toList()));
+    }
+
+    @PostMapping("/create-user-by-role")
+    public ResponseEntity<UserDTO> createUserByRole(@RequestBody RegisterModel registerModel) {
+        return ResponseEntity.ok(modelMapper.map(userService.create(registerModel), UserDTO.class));
+    }
+
+}

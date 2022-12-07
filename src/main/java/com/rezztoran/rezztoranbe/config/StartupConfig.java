7cb938aaa1e97f745163bc54b0ef45b2d7bec478
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-12-07 19:54:17 +0300

    StartupConfig created

---
+++ src/main/java/com/rezztoran/rezztoranbe/config/StartupConfig.java
@@ -0,0 +1,20 @@
+package com.rezztoran.rezztoranbe.config;
+
+import com.rezztoran.rezztoranbe.model.Role;
+import com.rezztoran.rezztoranbe.model.User;
+import com.rezztoran.rezztoranbe.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StartupConfig implements CommandLineRunner {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        userService.create(User.builder().username("grkn").password("pass").mail("gurkan@mail").name("grkn").surname("ucar").build(), Role.ADMIN);
+    }
+}

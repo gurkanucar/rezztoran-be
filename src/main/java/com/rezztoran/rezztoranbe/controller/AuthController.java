aa0f714002a60939569c5f5e58eb6e2f59dd8a0f
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-12-07 19:37:23 +0300

    AuthController created

---
+++ src/main/java/com/rezztoran/rezztoranbe/controller/AuthController.java
@@ -0,0 +1,39 @@
+package com.rezztoran.rezztoranbe.controller;
+
+import com.rezztoran.rezztoranbe.dto.TokenDTO;
+import com.rezztoran.rezztoranbe.dto.UserDTO;
+import com.rezztoran.rezztoranbe.dto.request.LoginModel;
+import com.rezztoran.rezztoranbe.dto.request.RegisterModel;
+import com.rezztoran.rezztoranbe.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+    private final ModelMapper modelMapper;
+
+
+    @PostMapping("/register")
+    public ResponseEntity<UserDTO> register(@RequestBody RegisterModel registerModel) {
+        return ResponseEntity.ok(modelMapper.map(authService.tryRegister(registerModel), UserDTO.class));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenDTO> login(@RequestBody LoginModel loginModel) {
+        return ResponseEntity.ok(authService.tryLogin(loginModel));
+    }
+
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDTO> getMyself() {
+        return ResponseEntity.ok(modelMapper.map(authService.getAuthenticatedUser(), UserDTO.class));
+    }
+
+
+}

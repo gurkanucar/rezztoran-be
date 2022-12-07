57fb7ad24e00c3786e7d897b8815b7c7afee394e
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-12-07 19:34:40 +0300

    BeanConfig updated

--- src/main/java/com/rezztoran/rezztoranbe/config/BeanConfig.java
+++ src/main/java/com/rezztoran/rezztoranbe/config/BeanConfig.java
@@ -2,10 +2,17 @@ package com.rezztoran.rezztoranbe.config;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BeanConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 
     @Bean
     public ModelMapper modelMapper() {

8244b782a2bd677bfe3a7ae843a30a9023dd86e3
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-12-07 19:35:50 +0300

    Mail sender configured

---
+++ src/main/java/com/rezztoran/rezztoranbe/config/MailConfig.java
@@ -0,0 +1,39 @@
+package com.rezztoran.rezztoranbe.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Value("${mail.password}")
+    String password;
+
+    @Value("${mail.address}")
+    String mail;
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername(mail);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
+

003fd12440af0c6e20e602686fddc21a85527e02
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-12-19 14:20:07 +0300

    field exception handler implemented

--- src/main/java/com/rezztoran/rezztoranbe/exception/GlobalExceptionHandler.java
+++ src/main/java/com/rezztoran/rezztoranbe/exception/GlobalExceptionHandler.java
@@ -1,9 +1,13 @@
 package com.rezztoran.rezztoranbe.exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.HashMap;
@@ -11,6 +15,14 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(x -> errors.put(((FieldError) x).getField(), x.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(errors);
+    }
 
     @ExceptionHandler(CategoryNotFoundException.class)
     public ResponseEntity<?> categoryNotFoundException(CategoryNotFoundException exception) {

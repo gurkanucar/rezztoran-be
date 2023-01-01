fbf1bc904b6a0fbf313ec3309cd19170a4e7da6c
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-01-01 20:10:21 +0300

    Exception general handler updated

--- src/main/java/com/rezztoran/rezztoranbe/exception/GlobalExceptionHandler.java
+++ src/main/java/com/rezztoran/rezztoranbe/exception/GlobalExceptionHandler.java
@@ -17,54 +17,64 @@ import java.util.Map;
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
 
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach(x -> errors.put(((FieldError) x).getField(), x.getDefaultMessage()));
-        return ResponseEntity.badRequest().body(errors);
-    }
+  @Override
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+      HttpHeaders headers, HttpStatus status, WebRequest request) {
+    Map<String, String> errors = new HashMap<>();
+    ex.getBindingResult().getAllErrors()
+        .forEach(x -> errors.put(((FieldError) x).getField(), x.getDefaultMessage()));
+    return ResponseEntity.badRequest().body(errors);
+  }
 
-    @ExceptionHandler(CategoryNotFoundException.class)
-    public ResponseEntity<?> categoryNotFoundException(CategoryNotFoundException exception) {
-        HashMap<String, String> errors = new HashMap<>();
-        errors.put("error", exception.getMessage());
-        return ResponseEntity.status(404).body(errors);
-    }
+  @ExceptionHandler(CategoryNotFoundException.class)
+  public ResponseEntity<?> categoryNotFoundException(CategoryNotFoundException exception) {
+    HashMap<String, String> errors = new HashMap<>();
+    errors.put("error", exception.getMessage());
+    return ResponseEntity.status(404).body(errors);
+  }
 
-    @ExceptionHandler(CategoryAlreadyExistsException.class)
-    public ResponseEntity<?> categoryAlreadyExists(CategoryAlreadyExistsException exception) {
-        HashMap<String, String> errors = new HashMap<>();
-        errors.put("error", exception.getMessage());
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(errors);
-    }
+  @ExceptionHandler(CategoryAlreadyExistsException.class)
+  public ResponseEntity<?> categoryAlreadyExists(CategoryAlreadyExistsException exception) {
+    HashMap<String, String> errors = new HashMap<>();
+    errors.put("error", exception.getMessage());
+    return ResponseEntity.status(HttpStatus.CONFLICT).body(errors);
+  }
 
-    @ExceptionHandler(WrongCredientalsException.class)
-    public ResponseEntity<?> usernameOrPasswordInvalidException(WrongCredientalsException exception) {
-        Map<String, String> errors = new HashMap<>();
-        errors.put("error", exception.getMessage());
-        return new ResponseEntity<>(errors, HttpStatus.NOT_FOUND);
-    }
+  @ExceptionHandler(WrongCredientalsException.class)
+  public ResponseEntity<?> usernameOrPasswordInvalidException(WrongCredientalsException exception) {
+    Map<String, String> errors = new HashMap<>();
+    errors.put("error", exception.getMessage());
+    return new ResponseEntity<>(errors, HttpStatus.NOT_FOUND);
+  }
 
 
-    @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<?> userNotFoundException(UserNotFoundException exception) {
-        Map<String, String> errors = new HashMap<>();
-        errors.put("error", exception.getMessage());
-        return new ResponseEntity<>(errors, HttpStatus.NOT_FOUND);
-    }
+  @ExceptionHandler(UserNotFoundException.class)
+  public ResponseEntity<?> userNotFoundException(UserNotFoundException exception) {
+    Map<String, String> errors = new HashMap<>();
+    errors.put("error", exception.getMessage());
+    return new ResponseEntity<>(errors, HttpStatus.NOT_FOUND);
+  }
 
-    @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<?> notFoundException(NotFoundException exception) {
-        Map<String, String> errors = new HashMap<>();
-        errors.put("error", exception.getMessage());
-        return new ResponseEntity<>(errors, HttpStatus.NOT_FOUND);
-    }
+  @ExceptionHandler(NotFoundException.class)
+  public ResponseEntity<?> notFoundException(NotFoundException exception) {
+    Map<String, String> errors = new HashMap<>();
+    errors.put("error", exception.getMessage());
+    return new ResponseEntity<>(errors, HttpStatus.NOT_FOUND);
+  }
 
-    @ExceptionHandler(GenericErrorResponse.class)
-    public ResponseEntity<?> genericError(GenericErrorResponse exception) {
-        Map<String, String> errors = new HashMap<>();
-        errors.put("error", exception.getMessage());
-        return new ResponseEntity<>(errors, exception.getHttpStatus());
-    }
+  @ExceptionHandler(GenericErrorResponse.class)
+  public ResponseEntity<?> genericError(GenericErrorResponse exception) {
+    Map<String, String> errors = new HashMap<>();
+    errors.put("error", exception.getMessage());
+    return new ResponseEntity<>(errors, exception.getHttpStatus());
+  }
+
+
+  @ExceptionHandler(Exception.class)
+  public final ResponseEntity<?> handleAllException(Exception ex, WebRequest request) {
+    Map<String, String> errors = new HashMap<>();
+    errors.put("error", ex.getMessage());
+    return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+  }
 
 }

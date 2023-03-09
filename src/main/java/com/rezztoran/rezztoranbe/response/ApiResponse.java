a7b4ae0c92da2a9d9877d62316e37e2c394c3ac6
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-03-09 23:32:11 +0300

    ApiResponse update

--- src/main/java/com/rezztoran/rezztoranbe/response/ApiResponse.java
+++ src/main/java/com/rezztoran/rezztoranbe/response/ApiResponse.java
@@ -2,7 +2,6 @@ package com.rezztoran.rezztoranbe.response;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.rezztoran.rezztoranbe.enums.ResponseConstants;
 import lombok.Data;
@@ -11,27 +10,27 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 @Data
-@JsonInclude(Include.NON_NULL)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({"status", "code", "message", "isPageable", "content", "error"})
-public class ApiResponse {
+public class ApiResponse<T> {
 
   @JsonIgnore private HttpHeaders httpHeaders;
   private HttpStatus status;
   private int code;
   private String message;
   private Boolean isPageable;
-  private Object error;
-  private Object content;
+  private T error;
+  private T content;
 
-  public static Builder builder(HttpStatus status, ResponseConstants responseConstants) {
-    return new Builder(status, responseConstants);
+  public static <T> Builder<T> builder(HttpStatus status, ResponseConstants responseConstants) {
+    return new Builder<>(status, responseConstants);
   }
 
-  public static Builder builder() {
-    return new Builder();
+  public static <T> Builder<T> builder() {
+    return new Builder<>();
   }
 
-  private ApiResponse(Builder builder) {
+  private ApiResponse(Builder<T> builder) {
     this.status = builder.status;
     this.httpHeaders = builder.httpHeaders;
     this.code = builder.code;
@@ -41,47 +40,49 @@ public class ApiResponse {
     this.error = builder.error;
   }
 
-  public static class Builder {
+  public static class Builder<T> {
 
     private HttpHeaders httpHeaders = new HttpHeaders();
     private HttpStatus status;
     private int code;
-    private Boolean isPageable;
-    private Object content;
-    private Object error;
+    private Boolean isPageable = false;
+    private T content;
+    private T error;
     private String message;
 
     public Builder(HttpStatus status, ResponseConstants responseConstants) {
       this.status = status;
       this.code = responseConstants.getCode();
       this.message = responseConstants.getMessage();
-      this.isPageable = false;
     }
 
     public Builder() {
       this.status = HttpStatus.OK;
       this.code = ResponseConstants.SUCCESS.getCode();
       this.message = ResponseConstants.SUCCESS.getMessage();
-      this.isPageable = false;
     }
 
-    public Builder header(String name, String value) {
+    public Builder<T> header(String name, String value) {
       this.httpHeaders.add(name, value);
       return this;
     }
 
-    public Builder data(Object object) {
+    public Builder<T> data(T object) {
       this.content = object;
       return this;
     }
 
-    public Builder pageableData(Object object) {
+    public Builder<T> pageableData(T object) {
       this.isPageable = true;
       this.content = object;
       return this;
     }
 
-    public Builder error(Object obj, HttpStatus status) {
+    public Builder<T> error(T obj) {
+      return error(obj, HttpStatus.BAD_REQUEST);
+    }
+
+    public Builder<T> error(T obj, HttpStatus status) {
       this.code = ResponseConstants.FAILURE.getCode();
       this.status = status;
       this.message = null;
@@ -90,8 +91,8 @@ public class ApiResponse {
       return this;
     }
 
-    public ResponseEntity<ApiResponse> build() {
-      ApiResponse apiResponse = new ApiResponse(this);
+    public ResponseEntity<ApiResponse<T>> build() {
+      ApiResponse<T> apiResponse = new ApiResponse<>(this);
       return ResponseEntity.status(status).headers(httpHeaders).body(apiResponse);
     }
   }

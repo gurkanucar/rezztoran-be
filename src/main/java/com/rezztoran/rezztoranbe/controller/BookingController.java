7b8584dd3c0234b5f142ce74ce24f1b065e89096
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-03-13 23:31:08 +0300

    Booking update endpoint created

--- src/main/java/com/rezztoran/rezztoranbe/controller/BookingController.java
+++ src/main/java/com/rezztoran/rezztoranbe/controller/BookingController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,5 +34,11 @@ public class BookingController {
   public ResponseEntity<ApiResponse<Object>> createBooking(
       @RequestBody BookRequestModel bookRequestModel) {
     return ApiResponse.builder().data(bookService.createBook(bookRequestModel)).build();
+  }
+
+  @PutMapping
+  public ResponseEntity<ApiResponse<Object>> updateBooking(
+      @RequestBody BookRequestModel bookRequestModel) {
+    return ApiResponse.builder().data(bookService.updateBook(bookRequestModel)).build();
   }
 }

4ca211e28cf763c9d98f350c233a7620769dc2bd
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-03-13 21:58:00 +0300

    BookRequestModel created

---
+++ src/main/java/com/rezztoran/rezztoranbe/dto/request/BookRequestModel.java
@@ -0,0 +1,23 @@
+package com.rezztoran.rezztoranbe.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import javax.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class BookRequestModel {
+
+  @NotBlank private Long userId;
+
+  @NotBlank private Long restaurantId;
+
+  @NotBlank
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+  private LocalDate reservationDate;
+
+  @NotBlank
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
+  private LocalTime reservationTime;
+}

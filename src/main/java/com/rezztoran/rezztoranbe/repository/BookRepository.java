6c163b07afc183984e4c6b70c5e834b79175aef5
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-03-14 00:52:24 +0300

    new booking queries created

--- src/main/java/com/rezztoran/rezztoranbe/repository/BookRepository.java
+++ src/main/java/com/rezztoran/rezztoranbe/repository/BookRepository.java
@@ -1,5 +1,6 @@
 package com.rezztoran.rezztoranbe.repository;
 
+import com.rezztoran.rezztoranbe.enums.BookingStatus;
 import com.rezztoran.rezztoranbe.model.Booking;
 import java.time.LocalDate;
 import java.util.List;
@@ -8,6 +9,15 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookRepository extends JpaRepository<Booking, Long> {
+
+  List<Booking> findAllByUser_IdAndReservationDate(Long id, LocalDate localDate);
+  List<Booking> findAllByUser_Id(Long id);
+
+  List<Booking> findAllByRestaurant_IdAndReservationDateAndBookingStatus(
+      Long id, LocalDate localDate, BookingStatus bookingStatus);
+
+  List<Booking> findAllByRestaurant_IdAndReservationDateAndBookingStatusNot(
+      Long id, LocalDate localDate, BookingStatus bookingStatus);
 
   List<Booking> findAllByRestaurant_IdAndReservationDate(Long id, LocalDate localDate);
 }

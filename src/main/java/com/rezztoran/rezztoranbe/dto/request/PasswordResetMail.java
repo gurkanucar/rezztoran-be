62956abb65117258dcba46c9908322516619a035
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-03-14 10:55:03 +0300

    PasswordResetMail class created

---
+++ src/main/java/com/rezztoran/rezztoranbe/dto/request/PasswordResetMail.java
@@ -0,0 +1,18 @@
+package com.rezztoran.rezztoranbe.dto.request;
+
+import javax.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PasswordResetMail {
+  private MailModel mailModel;
+  private String username;
+  @NotBlank private String mail;
+  @NotBlank private Integer code;
+}

2f7b519d070f1dcab32fcb5afd40352942251002
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-03-14 03:12:24 +0300

    AsyncTaskExecutor created

---
+++ src/main/java/com/rezztoran/rezztoranbe/config/AsyncTaskExecutor.java
@@ -0,0 +1,20 @@
+package com.rezztoran.rezztoranbe.config;
+
+import java.util.concurrent.Executor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+public class AsyncTaskExecutor {
+  @Bean
+  public Executor asyncExecutor() {
+    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+    executor.setCorePoolSize(2);
+    executor.setMaxPoolSize(2);
+    executor.setQueueCapacity(500);
+    executor.setThreadNamePrefix("JDAsync-");
+    executor.initialize();
+    return executor;
+  }
+}

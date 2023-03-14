b394282ecf1083fedb5047b253393e2d298fb03f
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-03-14 15:29:30 +0300

    StoredFile model created

---
+++ src/main/java/com/rezztoran/rezztoranbe/model/StoredFile.java
@@ -0,0 +1,19 @@
+package com.rezztoran.rezztoranbe.model;
+
+import java.io.InputStream;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class StoredFile {
+
+  private String id;
+  private String fileName;
+  private Long contentLength;
+  private InputStream inputStream;
+}

9c82adf145358fc3ef0dd7542c25de7b45a65f46
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-03-16 13:56:14 +0300

    ReviewRequestModel updated

--- src/main/java/com/rezztoran/rezztoranbe/dto/request/ReviewRequestModel.java
+++ src/main/java/com/rezztoran/rezztoranbe/dto/request/ReviewRequestModel.java
@@ -1,5 +1,9 @@
 package com.rezztoran.rezztoranbe.dto.request;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,8 +16,14 @@ import lombok.NoArgsConstructor;
 public class ReviewRequestModel {
 
   private Long id;
-  private Long restaurantId;
-  private Long userId;
-  private String content;
+
+  @NotNull private Long restaurantId;
+
+  @NotNull private Long userId;
+
+  @NotBlank private String content;
+
+  @Min(0)
+  @Max(5)
   private int star;
 }

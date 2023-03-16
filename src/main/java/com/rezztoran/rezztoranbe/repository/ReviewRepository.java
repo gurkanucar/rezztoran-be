403502c5ae2902f63236aec083d126370ee2a9cc
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-03-16 13:55:26 +0300

    ReviewRepository created

--- src/main/java/com/rezztoran/rezztoranbe/repository/ReviewRepository.java
+++ src/main/java/com/rezztoran/rezztoranbe/repository/ReviewRepository.java
@@ -1,9 +1,21 @@
 package com.rezztoran.rezztoranbe.repository;
 
 import com.rezztoran.rezztoranbe.model.Review;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /** The interface Review repository. */
 @Repository
-public interface ReviewRepository extends JpaRepository<Review, Long> {}
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+  List<Review> findAllByUser_Id(Long userId);
+
+  List<Review> findAllByRestaurant_Id(Long restaurantId);
+
+
+  @Query("SELECT e FROM Review e WHERE e.restaurant.id IN :restaurantIds")
+  List<Review> findAllByRestaurantIds(@Param("restaurantIds") List<Long> restaurantIds);
+}

f6db3e13c8aa96b18642308ce02428e3987c106e
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-03-16 13:56:30 +0300

    ReviewController updated

--- src/main/java/com/rezztoran/rezztoranbe/controller/ReviewController.java
+++ src/main/java/com/rezztoran/rezztoranbe/controller/ReviewController.java
@@ -2,6 +2,8 @@ package com.rezztoran.rezztoranbe.controller;
 
 import com.rezztoran.rezztoranbe.dto.request.ReviewRequestModel;
 import com.rezztoran.rezztoranbe.response.ApiResponse;
+import com.rezztoran.rezztoranbe.service.ReviewService;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,28 +20,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class ReviewController {
 
+  private final ReviewService reviewService;
+
   @PostMapping
-  public ResponseEntity<ApiResponse<Object>> createReview(@RequestBody ReviewRequestModel request) {
-    return ApiResponse.builder().build();
+  public ResponseEntity<ApiResponse<Object>> createReview(@Valid @RequestBody ReviewRequestModel request) {
+    return ApiResponse.builder().data(reviewService.createReview(request)).build();
   }
 
   @PutMapping
-  public ResponseEntity<ApiResponse<Object>> updateReview(@RequestBody ReviewRequestModel request) {
-    return ApiResponse.builder().build();
+  public ResponseEntity<ApiResponse<Object>> updateReview(@Valid @RequestBody ReviewRequestModel request) {
+    return ApiResponse.builder().data(reviewService.updateReview(request)).build();
   }
 
   @DeleteMapping("/{id}")
   public ResponseEntity<ApiResponse<Object>> deleteReviewById(@PathVariable Long id) {
+    reviewService.deleteReviewById(id);
     return ApiResponse.builder().build();
   }
 
   @GetMapping("/restaurant/{id}")
-  public ResponseEntity<ApiResponse<Object>> getCommentsByRestaurant(@PathVariable Long id) {
-    return ApiResponse.builder().build();
+  public ResponseEntity<ApiResponse<Object>> getReviewsByRestaurant(@PathVariable Long id) {
+    return ApiResponse.builder().data(reviewService.getReviewsByRestaurant(id)).build();
   }
 
   @GetMapping("/user/{id}")
-  public ResponseEntity<ApiResponse<Object>> getCommentsByUser(@PathVariable Long id) {
-    return ApiResponse.builder().build();
+  public ResponseEntity<ApiResponse<Object>> getReviewsByUser(@PathVariable Long id) {
+    return ApiResponse.builder().data(reviewService.getReviewsByUser(id)).build();
   }
 }

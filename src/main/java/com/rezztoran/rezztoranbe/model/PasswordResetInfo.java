ec68cb91c5cb9d2192dbe706a5faffe45cc3efe9
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-04-18 13:59:40 +0300

    PasswordResetInfo model created

---
+++ src/main/java/com/rezztoran/rezztoranbe/model/PasswordResetInfo.java
@@ -0,0 +1,32 @@
+package com.rezztoran.rezztoranbe.model;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "password_reset_info")
+public class PasswordResetInfo  extends BaseEntity{
+
+  @Column(name = "reset_password", nullable = false, columnDefinition = "boolean default false")
+  private boolean resetPassword;
+
+
+  @Column(name = "reset_password_code")
+  private Integer resetPasswordCode;
+
+  @Column(name = "reset_password_expiration")
+  private LocalDateTime resetPasswordExpiration;
+
+  @OneToOne(mappedBy = "passwordResetInfo")
+  private User user;
+
+}

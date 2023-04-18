ca424d6635f838c2eeee3dec0240b45b541fa63c
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-04-18 13:58:35 +0300

    Exception messages updated

--- src/main/java/com/rezztoran/rezztoranbe/exception/BusinessException.java
+++ src/main/java/com/rezztoran/rezztoranbe/exception/BusinessException.java
@@ -23,49 +23,79 @@ public class BusinessException extends RuntimeException {
   public enum Ex {
     /** Default exception ex. */
     DEFAULT_EXCEPTION("messages.error.default_message", HttpStatus.BAD_REQUEST),
+
     /** Already exists exception ex. */
     ALREADY_EXISTS_EXCEPTION("messages.error.already_exists_exception", HttpStatus.BAD_REQUEST),
+
     /** Not found exception ex. */
     NOT_FOUND_EXCEPTION("messages.error.not_found_exception", HttpStatus.NOT_FOUND),
+
     /** User not found exception ex. */
     USER_NOT_FOUND_EXCEPTION("messages.error.user_not_found_exception", HttpStatus.NOT_FOUND),
+
     /** Access denied exception ex. */
     ACCESS_DENIED_EXCEPTION("messages.error.access_denied_exception", HttpStatus.UNAUTHORIZED),
+
     /** Forbidden exception ex. */
     FORBIDDEN_EXCEPTION("messages.error.forbidden_exception", HttpStatus.FORBIDDEN),
+
     /** Could not book exception ex. */
     COULD_NOT_BOOK_EXCEPTION("messages.error.could_not_book_exception", HttpStatus.BAD_REQUEST),
+
     /** Book not found exception ex. */
     BOOK_NOT_FOUND_EXCEPTION("messages.error.book_not_found_exception", HttpStatus.NOT_FOUND),
+
     /** Available book not found exception ex. */
     AVAILABLE_BOOK_NOT_FOUND_EXCEPTION(
         "messages.error.available_book_not_found_exception", HttpStatus.NOT_FOUND),
+
     /** Food not found exception ex. */
     FOOD_NOT_FOUND_EXCEPTION("messages.error.food_not_found_exception", HttpStatus.NOT_FOUND),
+
     /** User already owner of a restaurant exception ex. */
     USER_ALREADY_OWNER_OF_A_RESTAURANT_EXCEPTION(
         "messages.error.user_already_owner_of_a_restaurant_exception", HttpStatus.BAD_REQUEST),
+
     /** Restaurant not found exception ex. */
     RESTAURANT_NOT_FOUND_EXCEPTION(
         "messages.error.restaurant_not_found_exception", HttpStatus.NOT_FOUND),
+
     /** Wrong credentials exception ex. */
     WRONG_CREDENTIALS_EXCEPTION("messages.error.wrong_credentials_exception", HttpStatus.FORBIDDEN),
+
     /** Menu not found exception ex. */
     MENU_NOT_FOUND_EXCEPTION("messages.error.menu_not_found_exception", HttpStatus.NOT_FOUND),
+
     /** Restaurant already exists exception ex. */
     RESTAURANT_ALREADY_EXISTS_EXCEPTION(
         "messages.error.restaurant_already_exists_exception", HttpStatus.BAD_REQUEST),
+
     /** User already exists exception ex. */
     USER_ALREADY_EXISTS_EXCEPTION(
         "messages.error.user_already_exists_exception", HttpStatus.BAD_REQUEST),
+
     /** Category not found exception ex. */
     CATEGORY_NOT_FOUND_EXCEPTION(
         "messages.error.category_not_found_exception", HttpStatus.NOT_FOUND),
+
     /** Category already exists exception ex. */
     CATEGORY_ALREADY_EXISTS_EXCEPTION(
         "messages.error.category_already_exists_exception", HttpStatus.NOT_FOUND),
+    /** Password reset request not found exception ex. */
+    PASSWORD_RESET_REQUEST_NOT_FOUND_EXCEPTION(
+        "messages.error.password_reset_request_code_not_found_exception", HttpStatus.NOT_FOUND),
+
+    /** Password reset request code wrong exception ex. */
+    PASSWORD_RESET_REQUEST_CODE_WRONG_EXCEPTION(
+        "messages.error.password_reset_request_code_wrong_exception", HttpStatus.NOT_FOUND),
+
+    /** Password reset request code expired exception ex. */
+    PASSWORD_RESET_REQUEST_CODE_EXPIRED_EXCEPTION(
+        "messages.error.password_reset_request_code_expired_exception", HttpStatus.NOT_FOUND),
+
     /** Review not found exception ex. */
     REVIEW_NOT_FOUND_EXCEPTION("messages.error.review_not_found_exception", HttpStatus.NOT_FOUND),
+
     /** Review already exists exception ex. */
     REVIEW_ALREADY_EXISTS_EXCEPTION(
         "messages.error.review_already_exists_exception", HttpStatus.BAD_REQUEST);

4b37c19e553afa9d1a8ee26f46e960e53b1843b3
Hasanfener123 <maykil_jordan2@hotmail.com>
2023-05-21 19:33:47 +0300

    code reformatted

--- src/main/java/com/rezztoran/rezztoranbe/service/TokenService.java
+++ src/main/java/com/rezztoran/rezztoranbe/service/TokenService.java
@@ -1,24 +1,10 @@
 package com.rezztoran.rezztoranbe.service;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.rezztoran.rezztoranbe.model.User;
-
-import java.nio.charset.StandardCharsets;
-import java.util.Date;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Service;
-
-
 public interface TokenService {
 
-    String generateToken(Authentication auth);
+  String generateToken(Authentication auth);
 
-    DecodedJWT verifyJWT(String token);
-
+  DecodedJWT verifyJWT(String token);
 }

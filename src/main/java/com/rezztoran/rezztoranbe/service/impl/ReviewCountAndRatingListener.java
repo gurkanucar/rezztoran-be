0aab07b508d36aece04a20ac469e1be794d14b33
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-06-03 13:31:42 +0300

    fix: ReviewEntityListener small bug fixed

--- src/main/java/com/rezztoran/rezztoranbe/service/impl/ReviewCountAndRatingListener.java
+++ src/main/java/com/rezztoran/rezztoranbe/service/impl/ReviewCountAndRatingListener.java
@@ -4,6 +4,9 @@ import com.rezztoran.rezztoranbe.dto.ReviewDTO;
 import com.rezztoran.rezztoranbe.model.Review;
 import com.rezztoran.rezztoranbe.service.RestaurantService;
 import javax.persistence.EntityManager;
+import javax.persistence.PostPersist;
+import javax.persistence.PostRemove;
+import javax.persistence.PostUpdate;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -22,6 +25,9 @@ public class ReviewCountAndRatingListener implements ApplicationContextAware {
     context = applicationContext;
   }
 
+  @PostPersist
+  @PostUpdate
+  @PostRemove
   @Transactional(propagation = Propagation.REQUIRES_NEW)
   public void calculateAverageStarAndReviewCount(Review review) {
 
